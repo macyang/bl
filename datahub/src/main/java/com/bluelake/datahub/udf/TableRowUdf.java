1e8d955dd37236853890fa3ec10a9cc6060e994f
Mac Yang <mac.yang@gmail.com>
2014-03-01 17:47:04 -0800

    define bq processor interface

--- datahub/src/main/java/com/bluelake/datahub/udf/TableRowUdf.java
+++ datahub/src/main/java/com/bluelake/datahub/udf/TableRowUdf.java
@@ -1,8 +1,14 @@
 package com.bluelake.datahub.udf;
 
+import java.util.List;
+
+import com.google.api.services.bigquery.model.TableFieldSchema;
+import com.google.api.services.bigquery.model.TableRow;
+
 /*
  * Process a single BigQuery table row.
  */
 public interface TableRowUdf {
 
+  public void processTableRow(List<TableFieldSchema> fieldSchema, TableRow row);
 }
--- datahub/src/main/java/com/bluelake/datahub/udf/TableSplitUdf.java
+++ datahub/src/main/java/com/bluelake/datahub/udf/TableSplitUdf.java
@@ -1,8 +1,11 @@
 package com.bluelake.datahub.udf;
 
+import com.google.api.services.bigquery.model.GetQueryResultsResponse;
+
 /*
  * Process a single BigQuery table split task.
  */
 public interface TableSplitUdf {
 
+  public void processTableSplit(GetQueryResultsResponse queryResult);
 }
